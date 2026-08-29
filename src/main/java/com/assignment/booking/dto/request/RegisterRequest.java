d30c85001b453a184b717c56f4ec2d44dcdcc5bf
Shanmukha Prajwal <shanmukhaprajwal1122@gmail.com>
2026-08-29 22:45:52 +0530

    Add DTO layer - Request and Response DTOs with validation

---
+++ src/main/java/com/assignment/booking/dto/request/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.assignment.booking.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private String email;
+
+    private String fullName;
+}
---
+++ src/main/java/com/assignment/booking/dto/request/ReservationRequest.java
@@ -0,0 +1,36 @@
+package com.assignment.booking.dto.request;
+
+import com.assignment.booking.enums.ReservationStatus;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReservationRequest {
+
+    @NotNull(message = "Resource ID is required")
+    private Long resourceId;
+
+    @NotNull(message = "Start time is required")
+    @Future(message = "Start time must be in the future")
+    private LocalDateTime startTime;
+
+    @NotNull(message = "End time is required")
+    private LocalDateTime endTime;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.01", message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    private ReservationStatus status;
+
+    private String notes;
+}
